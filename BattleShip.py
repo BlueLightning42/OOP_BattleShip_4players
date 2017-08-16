5f0b8ca4c84620c4afeafe0f4325fa03644c6ee2
Blue Lightning <jawg45@live.com>
2017-08-16 15:01:17 -0400

    after my initial frenzied burst of activity updates will slow down...potentially to a crawl.
    >_<

--- BattleShip.py
+++ BattleShip.py
@@ -1,29 +1,35 @@
 #Main file 
 
-import player
+import Player
+import ShipBoard
 
-set_up();
+#set up all players
+all_players = set_up();
+#play game
 main_loop();
+#terminate
 end_program();
 
 #currently using placeholder names for planned functions may change at future points
-all_players = []
+
 
 def set_up():
 	#change default size and num?
 	SIZE = 8
 	NUMBER_OF_PLAYERS = 4
-
-	all_players.append(User(1))
+	
+	#looks suspiciously like the factory pattern...hmmmm is this unpythonic?
+	all_players = [User(1)]
 	for bot in range(2, NUMBER_OF_PLAYERS):
 		all_players.append(AI(bot, SIZE, NUMBER_OF_PLAYERS))
 	#figure out how to draw a gameboard (tkinter?)
-	
+	return all_players
 		
 def main_loop():
 	while len(all_players) > 1:
 	for p in all_players:
 		p.attack(all_players)
+		#update game board?
 	
 	
 def end_program():	
