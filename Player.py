9a7101541230fadb735ad51044a09e79a3a31a1d
Blue Lightning <jawg45@live.com>
2017-08-16 13:35:08 -0400

    Added AI Algorithm
    I haven't actually tested this so I Don't know if this works yet...
    I haven't tested it but in theory it should be beautiful 
    
    (basically copy pasted the algorithm from the C++ project I did then changed a few things like the struct is now a tuple of tuples and stuff...
    also used a list and a random choice from it instead of the messed up stuff I was doing before *shudders*)

--- Player.py
+++ Player.py
@@ -1,8 +1,10 @@
 #player class including both the ai and the user
+import random
 HIT = True
 MISS = False
 EMPTY = None
 SHIP = True
+DIRRECTIONS = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
 class Player:
 	'''abstract base class'''
@@ -12,16 +14,59 @@ class Player:
 		self.player_number = player_number
 		self.size = SIZE
 		
+		#Initializes a game board
+		self.ocean_board = [[MISS for i in range(size+2)] if (not x or not (size-x+1)) else [MISS if (not i or not (size-i+1)) else EMPTY for i in range(size+2)] for x in range(size+2)]
+		
+		#Initialise the Empty secret ship board
+		self._ship_board = [[MISS for i in range(size)] for x in range(size)]
+		
+		
 	def guess(self):
-		#random unoptimal guess
-		pass
+		#random unoptimal guess (making a stupid ai?)
+		while true:
+			row,cell = randint(1, SIZE), randint(1, SIZE);
+			if self.ocean_board[row][cell] is EMPTY:
+				return row,cell
 	
+	#Don't know if this works yet...I haven't tested it but in theory it should be beautiful
 	def generate_probability(self):
-		#smart guessing
-		pass
+		best_choice = (1,1) #default to be replaced
+		similar_probabilities = []
+		higest_probability = 0
+		#check each cell
+		for row in range(1,size-1):
+			for cell in range(1,size-1):
+				#skip cells that are already hit
+				if self.ocean_board[row][cell] == HIT: continue
+				
+				probability=0
+				#check in a direction
+				def look_dir(direc, row_, cell_):
+					prob = 0
+					for j in range(1, LARGEST_BOAT):
+						if ocean_board[row_ + direc[0]*j][cell_ + direc[1]*j] is EMPTY:
+							prob += (LARGEST_BOAT+1-j)
+						else: break
+					return prob
+				
+				look_dir(DIRRECTIONS[0], row, cell) #up
+				look_dir(DIRRECTIONS[1], row, cell) #down
+				look_dir(DIRRECTIONS[2], row, cell) #left
+				look_dir(DIRRECTIONS[3], row, cell) #right
+
+				#decide if stronger probability
+				if probability > higest_probability:
+					similar_probabilities = [(row,cell)]
+					higest_probability = probability
+				elif probability == higest_probability:
+					similar_probabilities.append((row,cell))
+
+		#then chose which probability to use
+		best_choice = random.choice(similar_probabilities)
+		return best_choice
 	
 	def search(self):
-		#if player is hit
+		#if player is hit search
 		pass
 	
 	def hit(self, x, y):
