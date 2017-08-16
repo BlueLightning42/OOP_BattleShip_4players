0c733f98a8ad340a2c597fb198f74cac91c53c30
Blue Lightning <jawg45@live.com>
2017-08-16 14:53:18 -0400

    created file for future use
    file is empty...TODO figure out how to program a gui...and how I want to set it up.

---
+++ PlayingField.py
@@ -0,0 +1,8 @@
+#board.py
+
+#to do...absolutly everything starting from how to do gui programming.
+
+
+
+#main ideas either set up a table for each player that can track which cells are clicked
+#OR just set up a 2d array of buttons and figure out how to link them to the "ship_board" list for each player
