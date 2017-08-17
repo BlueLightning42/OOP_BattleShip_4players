9b263d6a6a89f028208e032ab0d674f3a3a412a3
Blue Lightning <jawg45@live.com>
2017-08-17 15:15:59 -0400

    changed initialization
    changed what is being sent to the initialization

--- BattleShip.py
+++ BattleShip.py
@@ -20,16 +20,16 @@ def set_up():
 	
 	#looks suspiciously like the factory pattern...hmmmm is this unpythonic?
 	all_players = [User(1)]
-	for bot in range(2, NUMBER_OF_PLAYERS):
+	for bot in range(2, SIZE, NUMBER_OF_PLAYERS):
 		all_players.append(AI(bot, SIZE, NUMBER_OF_PLAYERS))
 	#figure out how to draw a gameboard (tkinter?)
 	return all_players
 		
 def main_loop():
 	while len(all_players) > 1:
-	for p in all_players:
-		p.attack(all_players)
-		#update game board?
+		for p in all_players:
+			p.attack(all_players)
+			#update game board?
 	
 	
 def end_program():	
