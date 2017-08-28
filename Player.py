e9c11859be05e89dbfe8b4c460eb998a06c1cd98
Blue Ϟ Lightning <jawg45@live.com>
2017-08-28 10:35:32 -0400

    added doc strings
    added descriptions of functions

--- Player.py
+++ Player.py
@@ -13,6 +13,7 @@ class Player:
 	'''abstract base class'''
 	all_boats = [5,4,3,3,2]
 	def __init__(self, player_number):
+		'''initialize all atributes every player shares'''
 		self.LARGEST_BOAT = 5
 		self.player_number = player_number
 		self.is_hit = False
@@ -23,10 +24,12 @@ class Player:
 		
 		#Initialise the Empty secret ship board
 		self._ship_board = [[MISS for i in range(SIZE)] for x in range(SIZE)]
-		
+
+		self.ship_cells_left = sum(all_boats)
 		
 	def guess(self):
-		#random unoptimal guess
+		'''This is so the code doesn't look as mechanical
+		-ocationally have the AI make a radom unoptimal guess'''
 		while True:
 			row,cell = randint(1, SIZE), randint(1, SIZE);
 			if self.ocean_board[row][cell] is EMPTY:
@@ -34,9 +37,12 @@ class Player:
 	
 	#Don't know if this works yet...I haven't tested it but in theory it should be beautiful
 	def generate_probability(self):
+		'''function that finds the probability of each tile being a ship
+		then randomly choses one of those tiles to return'''
 		best_choice = (1,1) #default to be replaced
 		similar_probabilities = []
 		higest_probability = 0
+		
 		#check each cell
 		for row in range(1, SIZE-1):
 			for cell in range(1, SIZE-1):
@@ -44,7 +50,7 @@ class Player:
 				if self.ocean_board[row][cell] is not EMPTY: continue
 					
 				probability=0
-				#add bias to middles
+				#add bias to middles...fixes distribution
 				if self.ocean_board[row+1][cell] is EMPTY and self.ocean_board[row-1][cell] is EMPTY:
 					probability += 2
 				if self.ocean_board[row][cell+1] is EMPTY and self.ocean_board[row][cell-1] is EMPTY:
@@ -53,7 +59,9 @@ class Player:
 				#check all dirrections
 				for j in range(1, self.LARGEST_BOAT):
 					if self.ocean_board[row + j][cell] is EMPTY:
+						#add a larger probability to closer tiles
 						probability += (self.LARGEST_BOAT+1-j)
+					#If it finds a cell that has already been hit stop looking.
 					else: break
 				for j in range(1, self.LARGEST_BOAT):
 					if self.ocean_board[row][cell + j] is EMPTY:
@@ -80,7 +88,8 @@ class Player:
 		return best_choice
 	
 	def search(self):
-		#if player is hit search
+		'''function to call if the player has been hit-
+		ai will attempt to search in a line to find the ship'''
 		x,y = self.last_hit
 		
 		if self.ocean_board[x][y-1] or self.ocean_board[x+1][y] or not (self.ocean_board[x-1][y] and self.ocean_board[x+1][y]) :
@@ -106,6 +115,7 @@ class Player:
 		return self.generate_probability()
 	
 	def hit(self, x, y):
+		'''hit the ocean board as well as return whether it was sucessfull or not'''
 		if self._ship_board[x-1][y-1]:
 			self.is_hit = True
 			self.ship_cells_left -= 1
@@ -125,11 +135,7 @@ class User(Player):
 	'''the person playing the game'''
 	def __init__(self, player_number):
 		super().__init__(player_number)
-		self._ship_board = [[MISS for i in range(SIZE)] for x in range(SIZE)]
-		#find user input
-		
-		self.ship_cells_left = sum(all_boats)
-		
+		#Get user input from the form.
 		
 	def attack(self, all_players):
 		#get user input for example
@@ -148,11 +154,7 @@ class User(Player):
 class AI(Player):
 	'''computer the user plays againsed'''
 	def __init__(self, player_number):
-		
 		super().__init__(player_number)
-		self._ship_board = [[MISS for i in range(SIZE)] for x in range(SIZE)]
-		#standard set up
-		self.ship_cells_left = sum(all_boats)
 		
 		for boat in all_boats:
 			while True:
@@ -176,6 +178,8 @@ class AI(Player):
 			self._ship_board[col + i*direction[0]][row + i*direction[1]] = SHIP
 	
 	def attack(self, all_players):
+		'''function called for every ai instance
+		-will chose who to attack then hit them'''
 		chosen_victim = PlayerRegistry.pick_oponent(self.player_number):
 		if all_players[chosen_victim].is_hit:
 			x,y = all_players[chosen_victim].search()
@@ -193,7 +197,7 @@ class AI(Player):
 	
 				
 class DeadPlayer:
-	'''empty class for dead people'''
+	'''Empty class for dead players'''
 	def __init__(self):
 		self.ship_cells_left == 0
 
