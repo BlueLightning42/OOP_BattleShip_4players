50c6cbe9761fedd068f7e1d5e9b3898d326e28eb
Blue Ϟ Lightning <jawg45@live.com>
2017-08-28 12:02:56 -0400

    still don't know what I'm doing
    made a bit of a template for the board

--- PlayingField.py
+++ PlayingField.py
@@ -6,3 +6,31 @@
 
 #main ideas either set up a table for each player that can track which cells are clicked
 #OR just set up a 2d array of buttons and figure out how to link them to the "ship_board" list for each player
+
+from PlayerRegistry import SIZE
+
+
+
+
+HIT = True
+MISS = False
+EMPTY = None
+
+class Tile():
+	'''ocean cell for the grid'''
+	def __init__(self):
+		self.state = EMPTY
+		return self
+	
+	
+class Grid():
+	'''grid for each player'''
+	def __init__(self)
+		self.ocean_grid = [[Tile() for i in range(SIZE)] for j in range(SIZE)
+		return self
+	
+	def update_grid(self, row, cell, has_hit):
+		if has_hit:
+			self.ocean_grid[row+1][cell+1].state = HIT
+		else:
+			self.ocean_grid[row+1][cell+1].state = MISS
